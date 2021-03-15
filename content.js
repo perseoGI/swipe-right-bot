d6dd439a9abfc218bf200ffaf671b30803835b4a
Perseo <perseo.gi98@gmail.com>
2021-03-15 10:01:45 +0100

    updated queryselector, CORS error

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 function swipeLeft() {
+  console.log("swipe left");
   let bnt = document.querySelector("button[aria-label='Nope']");
   if (bnt) {
     bnt.click();
@@ -14,28 +15,29 @@ function swipeRight(profileImageURL, why) {
 }
 
 function processImage(apiKey, profileImageURL) {
+  console.log("Processing image");
   let data = {
     requests: [
       {
         image: {
           source: {
-            imageUri: profileImageURL
-          }
+            imageUri: profileImageURL,
+          },
         },
         features: [
           {
-            type: "LABEL_DETECTION"
-          }
-        ]
-      }
-    ]
+            type: "LABEL_DETECTION",
+          },
+        ],
+      },
+    ],
   };
   let xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
 
   xhr.addEventListener("readystatechange", function () {
+    console.log( 'event listener' )
     if (this.readyState === 4 && this.status === 200) {
-
       let res = JSON.parse(this.responseText);
       let labels = res["responses"][0]["labelAnnotations"];
       if (labels === undefined) {
@@ -54,23 +56,35 @@ function processImage(apiKey, profileImageURL) {
         } else if ("selfie" in labelMap) {
           // I don't like it.
           swipeLeft();
-        } else if ("brassiere" in labelMap || "lingerie" in labelMap || "undergarment" in labelMap) {
+        } else if (
+          "brassiere" in labelMap ||
+          "lingerie" in labelMap ||
+          "undergarment" in labelMap
+        ) {
           // I don't like it.
           swipeLeft();
         } else if ("beauty" in labelMap && labelMap["beauty"] >= 0.9) {
           // Definitely swipe right. Never miss this.
           swipeRight(profileImageURL, "beauty");
-        } else if ("beauty" in labelMap && labelMap["beauty"] >= 0.8 &&
-          "smile" in labelMap && labelMap["smile"] >= 0.8) {
+        } else if (
+          "beauty" in labelMap &&
+          labelMap["beauty"] >= 0.8 &&
+          "smile" in labelMap &&
+          labelMap["smile"] >= 0.8
+        ) {
           // smile!
-          swipeRight(profileImageURL, 'smile');
-        } else if ("beauty" in labelMap && labelMap["beauty"] >= 0.8 &&
-          "hairstyle" in labelMap && labelMap["hairstyle"] >= 0.8) {
+          swipeRight(profileImageURL, "smile");
+        } else if (
+          "beauty" in labelMap &&
+          labelMap["beauty"] >= 0.8 &&
+          "hairstyle" in labelMap &&
+          labelMap["hairstyle"] >= 0.8
+        ) {
           // hairstyle!
-          swipeRight(profileImageURL, 'hairstyle');
+          swipeRight(profileImageURL, "hairstyle");
         } else if ("lady" in labelMap && labelMap["lady"] >= 0.8) {
           // lady!
-          swipeRight(profileImageURL, 'lady');
+          swipeRight(profileImageURL, "lady");
         } else {
           if (Math.random() * 100 <= Math.PI) {
             swipeRight(profileImageURL, "lucky");
@@ -88,36 +102,47 @@ function processImage(apiKey, profileImageURL) {
     }
   });
 
-  xhr.open("POST", "https://vision.googleapis.com/v1/images:annotate?key=" + apiKey);
+  xhr.open(
+    "POST",
+    "https://vision.googleapis.com/v1/images:annotate?key=" + apiKey
+  );
   xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
   xhr.setRequestHeader("cache-control", "no-cache");
+  // new
+  xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost");
 
   xhr.send(JSON.stringify(data));
-
 }
 
 chrome.storage.sync.get(null, function (items) {
   if (items.apiKey) {
     function mainLoop() {
       setTimeout(function () {
-        let imageNode = document.querySelectorAll('#content > span > div > div.App__body.H\\28 100\\25 \\29.Pos\\28 r\\29.Z\\28 0\\29 > div > main > div.H\\28 100\\25 \\29 > div > div > div.recsCardboard.W\\28 100\\25 \\29.Mt\\28 a\\29.H\\28 100\\25 \\29 --s.Px\\28 10px\\29 --s > div > div.recsCardboard__cards.Expand.Animdur\\28 \\24 fast\\29.Animtf\\28 eio\\29.Pos\\28 r\\29.CenterAlign.Z\\28 1\\29 > div.recCard.Ov\\28 h\\29.Cur\\28 p\\29.W\\28 100\\25 \\29.Bgc\\28 \\24 c-placeholder\\29.StretchedBox.Bdrs\\28 8px\\29.CenterAlign--ml.Toa\\28 n\\29.active > div.Expand.D\\28 f\\29.tappable-view > div.Animdur\\28 \\24 fast\\29.Animtf\\28 eio\\29.tappable_recCard.Expand.Pos\\28 a\\29 > div > div:nth-child(1) > div > div')[0];
+        // Right copy JS path
+        let imageNode = document.querySelector(
+          "#t-429325247 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.recsCardboard.W\\(100\\%\\).Mt\\(a\\).H\\(100\\%\\)--s.Px\\(4px\\)--s.Pos\\(r\\) > div > div.recsCardboard__cards.Expand.Animdur\\(\\$fast\\).Animtf\\(eio\\).Pos\\(r\\).CenterAlign > div.Toa\\(n\\).Wc\\(\\$transform\\).Prs\\(1000px\\).Bfv\\(h\\).Ov\\(h\\).W\\(100\\%\\).StretchedBox.Bgc\\(\\$c-placeholder\\).Bdrs\\(8px\\) > div.Expand.D\\(f\\).Pos\\(r\\).tappable-view.Cur\\(p\\) > div.Expand.Pos\\(a\\).D\\(f\\).Ov\\(h\\).Us\\(n\\).keen-slider > span:nth-child(1) > div"
+        );
         if (imageNode) {
           let imgUrl = imageNode.style.backgroundImage;
           imgUrl = imgUrl.substring(
-            imgUrl.indexOf("\"") + 1,
-            imgUrl.lastIndexOf("\"")
+            imgUrl.indexOf('"') + 1,
+            imgUrl.lastIndexOf('"')
           );
+          console.log(imgUrl);
           processImage(items.apiKey, imgUrl);
         } else {
-          console.log('Not found profile image, please refresh the page.');
+          console.log("Not found profile image, please refresh the page.");
         }
         mainLoop();
-      }, 3800)
+      }, 3800);
     }
 
     mainLoop();
   } else {
-    alert("Please add Google Vision API key from extension options. Now using demo mode!!");
+    alert(
+      "Please add Google Vision API key from extension options. Now using demo mode!!"
+    );
 
     function demoLoop() {
       setTimeout(function () {
@@ -127,7 +152,7 @@ chrome.storage.sync.get(null, function (items) {
           swipeLeft();
         }
         demoLoop();
-      }, 2000)
+      }, 2000);
     }
 
     demoLoop();
