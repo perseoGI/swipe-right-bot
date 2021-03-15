91a5a4b661c070a2f8d55b3e90dd0d53cc11ee1a
Perseo <perseo.gi98@gmail.com>
2021-03-15 10:19:22 +0100

    merged

--- content.js
+++ content.js
@@ -32,87 +32,78 @@ function processImage(apiKey, profileImageURL) {
       },
     ],
   };
-  let xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
 
-  xhr.addEventListener("readystatechange", function () {
-    console.log( 'event listener' )
-    if (this.readyState === 4 && this.status === 200) {
-      let res = JSON.parse(this.responseText);
-      let labels = res["responses"][0]["labelAnnotations"];
-      if (labels === undefined) {
-        swipeLeft();
-        return;
-      }
-      let labelMap = {};
-      for (let i = 0; i < labels.length; i++) {
-        labelMap[labels[i]["description"]] = labels[i]["score"];
-      }
-      console.log(labelMap);
-      if ("girl" in labelMap && labelMap["girl"] >= 0.6) {
-        if ("gravure idol" in labelMap || "japanese idol" in labelMap) {
-          // I don't like it.
-          swipeLeft();
-        } else if ("selfie" in labelMap) {
-          // I don't like it.
-          swipeLeft();
-        } else if (
-          "brassiere" in labelMap ||
-          "lingerie" in labelMap ||
-          "undergarment" in labelMap
-        ) {
-          // I don't like it.
-          swipeLeft();
-        } else if ("beauty" in labelMap && labelMap["beauty"] >= 0.9) {
-          // Definitely swipe right. Never miss this.
-          swipeRight(profileImageURL, "beauty");
-        } else if (
-          "beauty" in labelMap &&
-          labelMap["beauty"] >= 0.8 &&
-          "smile" in labelMap &&
-          labelMap["smile"] >= 0.8
-        ) {
-          // smile!
-          swipeRight(profileImageURL, "smile");
-        } else if (
-          "beauty" in labelMap &&
-          labelMap["beauty"] >= 0.8 &&
-          "hairstyle" in labelMap &&
-          labelMap["hairstyle"] >= 0.8
-        ) {
-          // hairstyle!
-          swipeRight(profileImageURL, "hairstyle");
-        } else if ("lady" in labelMap && labelMap["lady"] >= 0.8) {
-          // lady!
-          swipeRight(profileImageURL, "lady");
-        } else {
-          if (Math.random() * 100 <= Math.PI) {
-            swipeRight(profileImageURL, "lucky");
-          } else {
-            swipeLeft();
-          }
-        }
-      } else {
-        swipeLeft();
-      }
-    } else if (this.readyState === 4 && this.status !== 200) {
-      console.log(this.status);
-      console.log(this.responseText);
-      swipeLeft();
-    }
+
+  let headers = new Headers({
+    "Content-Type": "application/json; charset=utf-8",
+    "cache-control": "no-cache",
   });
+  let requestObject = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data)
+  }
+  let request = new Request('https://vision.googleapis.com/v1/images:annotate?key='+apiKey, requestObject);
+  fetch (request)
+    .then(data => console.log( data ))
+    .error(e => console.log( e))
 
-  xhr.open(
-    "POST",
-    "https://vision.googleapis.com/v1/images:annotate?key=" + apiKey
-  );
-  xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-  xhr.setRequestHeader("cache-control", "no-cache");
-  // new
-  xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost");
+  //xhr.addEventListener("readystatechange", function () {
+    //if (this.readyState === 4 && this.status === 200) {
 
-  xhr.send(JSON.stringify(data));
+      //let res = JSON.parse(this.responseText);
+      //let labels = res["responses"][0]["labelAnnotations"];
+      //if (labels === undefined) {
+        //swipeLeft();
+        //return;
+      //}
+      //let labelMap = {};
+      //for (let i = 0; i < labels.length; i++) {
+        //labelMap[labels[i]["description"]] = labels[i]["score"];
+      //}
+      //console.log(labelMap);
+      //if ("girl" in labelMap && labelMap["girl"] >= 0.6) {
+        //if ("gravure idol" in labelMap || "japanese idol" in labelMap) {
+          //// I don't like it.
+          //swipeLeft();
+        //} else if ("selfie" in labelMap) {
+          //// I don't like it.
+          //swipeLeft();
+        //} else if ("brassiere" in labelMap || "lingerie" in labelMap || "undergarment" in labelMap) {
+          //// I don't like it.
+          //swipeLeft();
+        //} else if ("beauty" in labelMap && labelMap["beauty"] >= 0.9) {
+          //// Definitely swipe right. Never miss this.
+          //swipeRight(profileImageURL, "beauty");
+        //} else if ("beauty" in labelMap && labelMap["beauty"] >= 0.8 &&
+          //"smile" in labelMap && labelMap["smile"] >= 0.8) {
+          //// smile!
+          //swipeRight(profileImageURL, 'smile');
+        //} else if ("beauty" in labelMap && labelMap["beauty"] >= 0.8 &&
+          //"hairstyle" in labelMap && labelMap["hairstyle"] >= 0.8) {
+          //// hairstyle!
+          //swipeRight(profileImageURL, 'hairstyle');
+        //} else if ("lady" in labelMap && labelMap["lady"] >= 0.8) {
+          //// lady!
+          //swipeRight(profileImageURL, 'lady');
+        //} else {
+          //if (Math.random() * 100 <= Math.PI) {
+            //swipeRight(profileImageURL, "lucky");
+          //} else {
+            //swipeLeft();
+          //}
+        //}
+      //} else {
+        //swipeLeft();
+      //}
+    //} else if (this.readyState === 4 && this.status !== 200) {
+      //console.log(this.status);
+      //console.log(this.responseText);
+      //swipeLeft();
+    //}
+  //});
+
+
 }
 
 chrome.storage.sync.get(null, function (items) {
